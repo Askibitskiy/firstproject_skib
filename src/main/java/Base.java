5847f88291c565ff18bd6e22eb90b7478946c394
Askibitskiy <askibitskiy@aplana.com>
2018-10-04 23:34:12 +0300

    доделал задание №3

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -11,12 +11,13 @@ public class Base {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Input first value/Введите первое число:");//вывод на экран строки с предложением ввести первое число
-        double a = scan.nextDouble();//ввод первого числа и присваивание значения переменной "а"
+            double a = scan.nextDouble();//ввод первого числа и присваивание значения переменной "а"
         System.out.println("Input second value/Введите второе число:");//вывод на экран строки с предложением ввести второе число
-        double b = scan.nextDouble();  //ввод второго числа и присваивание значения переменной "а"
-        double c = a + b; //сложение чисел
+            double b = scan.nextDouble();  //ввод второго числа и присваивание значения переменной "а"
+        System.out.println("Choose action(+-*/)/Выберите действие (+-*/):");//выбор действия с числами
+            double c = a + b; //сложение чисел
         String str = String.format("Результат=  %.4f", c); //присваивание переменной str форматированного значения переменной "с"
         println(str); //печать строки с результатоm
-        scan.close();
+        scan.close();}
     }
-}
+
